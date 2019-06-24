80ae60996cd76ca2fc1c97c3827850b9c0dcf8b8
Tristan Favor <tristanfavor29@gmail.com>
2019-06-24 17:17:58 -0500

    modified html and js

--- main.js
+++ main.js
@@ -8,27 +8,61 @@ $(function navbarToggle() {
     });
 })
 
+function showHeadPage () {
+    $("#head-page").removeClass('hidden');
+    $("#about-page").addClass('hidden');
+    $("#work-page").addClass('hidden');
+    $(".name").html('tristan favor');
+}
+
+function showAboutPage () {
+    $("#head-page").addClass('hidden');
+    $("#about-page").removeClass('hidden');
+    $("#work-page").addClass('hidden');
+    $(".name").html('about');
+}
+
+function showWorkPage () {
+    $("#head-page").addClass('hidden');
+    $("#about-page").addClass('hidden');
+    $("#work-page").removeClass('hidden');
+    $(".name").html('work');
+}
+
+
+function tristanLinkStyle() {
+    $(".tristan>a").css('font-weight', 'bolder');
+    $(".about>a").css('font-weight', '');
+    $(".work>a").css('font-weight', '');
+}
+
+function aboutLinkStyle() {
+    $(".about>a").css('font-weight', 'bolder');
+    $(".tristan>a").css('font-weight', '');
+    $(".work>a").css('font-weight', '');
+}
+
+function workLinkStyle() {
+    $(".work>a").css('font-weight', 'bolder');
+    $(".tristan>a").css('font-weight', '');
+    $(".about>a").css('font-weight', '');
+}
+
 
 $(function handleLinks() {
     $(".tristan").on('click', function(event) {
-        $("#head-page").removeClass('hidden');
-        $("#about-page").addClass('hidden');
-        $("#work-page").addClass('hidden');
-        $(".name").html('Tristan Favor');
+        showHeadPage ()
+        tristanLinkStyle()
         event.preventDefault();
     });
     $(".about").on('click', function(event) {
-        $("#head-page").addClass('hidden');
-        $("#about-page").removeClass('hidden');
-        $("#work-page").addClass('hidden');
-        $(".name").html('About');
+        showAboutPage ()
+        aboutLinkStyle()
         event.preventDefault();
     });
     $(".work").on('click', function(event) {
-        $("#head-page").addClass('hidden');
-        $("#about-page").addClass('hidden');
-        $("#work-page").removeClass('hidden');
-        $(".name").html('Work');
+        showWorkPage ()
+        workLinkStyle()
         event.preventDefault();
     });
 })
